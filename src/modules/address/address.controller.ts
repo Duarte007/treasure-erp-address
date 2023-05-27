fec29c69a4b0b6cc96f819d97e4518f3ea740236
Matheus Duarte <matheusdcarvalho007@gmail.com>
2023-05-27 17:42:14 -0300

    [feat] add endpoint to receive address event

--- src/modules/address/address.controller.ts
+++ src/modules/address/address.controller.ts
@@ -6,8 +6,12 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { TokenAuthQueryParam } from 'src/common/dtos/token-auth-qs.dto';
+import { PubsubPostBody } from '../../common/pubsub/interfaces/pubsub-post-body';
+import { PubSub } from '../../common/pubsub/pubsub.decorator';
 import { CreateAddressDTO } from './dto/create-address.dto';
 import { UpdateAddressDto } from './dto/update-address.dto';
 import { AddressService } from './services/addresses.service';
@@ -19,6 +23,17 @@ export class AddressController {
 
   @Post()
   create(@Body() createAddressDto: CreateAddressDTO) {
+    return this.addressService.create(createAddressDto);
+  }
+
+  @Post('event')
+  @PubSub()
+  createByEvent(
+    @Body() addressEvent: PubsubPostBody,
+    @Query() params: TokenAuthQueryParam,
+  ) {
+    const createAddressDto: CreateAddressDTO = addressEvent.message
+      .data as unknown as CreateAddressDTO;
     return this.addressService.create(createAddressDto);
   }
 
